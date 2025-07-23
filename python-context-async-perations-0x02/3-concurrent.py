70001879c151dc2270469e8b41ce2870e317b312
Kam Ephrem <kamnoah88@gmail.com>
2025-07-23 17:19:12 +0300

    feat(async): implement concurrent async queries with return values

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -6,18 +6,18 @@ async def async_fetch_users():
         async with db.execute("SELECT * FROM users") as cursor:
             rows = await cursor.fetchall()
             print("All Users:", rows)
-
+            return rows
 async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             rows = await cursor.fetchall()
             print("Users older than 40:", rows)
-
+            return rows
 async def fetch_concurrently():
     await asyncio.gather(
         async_fetch_users(),
         async_fetch_older_users()
     )
-
+    return results
 if __name__ == "__main__":
     asyncio.run(fetch_concurrently())
